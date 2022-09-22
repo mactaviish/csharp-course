c4659d935eb1ee2eacb340227f26e50193db4818
Leonardo Dalla Corte <leonardo.brancalione@estudante.uffs.edu.br>
2022-09-22 18:16:15 -0300

    aulas

--- Alura/Program.cs
+++ Alura/Program.cs
@@ -4,7 +4,19 @@ class Programa
 {
     static void Main(string[] args)
     {
+        int inteiro = 10;
+        int outroInteiro = 0;
+        float flutuante = 10.5f;
+        double outroFlutuante = 10.5;
+        
+        outroInteiro = (int)flutuante;
+
         Console.WriteLine("Hello, World!");
+        Console.WriteLine(inteiro);
+        Console.WriteLine(outroInteiro);
+        Console.WriteLine(flutuante);
+        Console.WriteLine(outroFlutuante);
+        Console.WriteLine("Tecle Enter para continuar...");
+        Console.ReadLine();
     }
 }
-
